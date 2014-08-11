1ab8bf81998104375aa0c85c8deac8c3a2f1c94b
liaoxuezhi <liaoxuezhi@baidu.com>
2014-08-11 14:26:22 +0800

    update

--- src/main/java/com/baidu/fis/velocity/spring/FisBean.java
+++ src/main/java/com/baidu/fis/velocity/spring/FisBean.java
@@ -1,7 +1,18 @@
 package com.baidu.fis.velocity.spring;
 
+import com.baidu.fis.velocity.util.Settings;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.servlet.ServletContext;
+
 /**
  * Created by 2betop on 8/11/14.
  */
-public class FisBean {
+public class FisBean implements ServletContextAware {
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        Settings.setApplicationAttribute(ServletContext.class.getName(), servletContext);
+        Settings.load(servletContext.getResourceAsStream(Settings.DEFAULT_PATH));
+    }
 }
