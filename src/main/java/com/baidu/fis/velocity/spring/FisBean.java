f71498c19b25cd3f094e653b5eafc1e149274e59
liaoxuezhi <liaoxuezhi@baidu.com>
2014-08-11 14:25:23 +0800

    update

---
+++ src/main/java/com/baidu/fis/velocity/spring/FisBean.java
@@ -0,0 +1,7 @@
+package com.baidu.fis.velocity.spring;
+
+/**
+ * Created by 2betop on 8/11/14.
+ */
+public class FisBean {
+}
--- src/main/java/com/baidu/fis/velocity/spring/controller/Home.java
+++
@@ -1,7 +0,0 @@
-package com.baidu.fis.velocity.spring.controller;
-
-/**
- * Created by 2betop on 8/11/14.
- */
-public class Home {
-}
